09e4162dc9de17820ae883548507d528b21d3585
hein <heincrebolder@gmail.com>
2017-12-13 11:01:01 +0100

    'yep'

--- src/app/components/characters/character-list/character-item/character-item.component.ts
+++ src/app/components/characters/character-list/character-item/character-item.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, ViewEncapsulation, Input, Output, EventEmitter } from '@angular/core';
-import { CharacterService } from '../../../../services/character.service';
 import { Character } from '../../../../models/character.model';
+import { CharacterService } from '../../../../services/character.service';
 @Component({
   selector: 'app-character-item',
   templateUrl: './character-item.component.html',
@@ -9,14 +9,10 @@ import { Character } from '../../../../models/character.model';
 export class CharacterItemComponent implements OnInit {
   @Input() character: Character;
   @Input() index: number;
-  
+
 
   constructor(private characterService: CharacterService) { }
 
   ngOnInit() {
-      console.dir(this.character);
   }
-
-
-
 }
--- src/app/components/characters/character-list/character-list.component.ts
+++ src/app/components/characters/character-list/character-list.component.ts
@@ -1,4 +1,3 @@
-
 import { Component, OnInit, OnDestroy, ViewEncapsulation, Output, EventEmitter } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { CharacterService } from '../../../services/character.service';
@@ -11,7 +10,7 @@ import { Subscription } from 'rxjs/Subscription';
   encapsulation: ViewEncapsulation.None
 })
 export class CharacterListComponent implements OnInit, OnDestroy {
-  character: Character[];
+  characters: Character[];
   subscription: Subscription;
 
   constructor(private characterService: CharacterService,
@@ -21,21 +20,23 @@ export class CharacterListComponent implements OnInit, OnDestroy {
 
   ngOnInit() {
       this.subscription = this.characterService.charactersChanged.subscribe(
-          (character: Character[]) => {
-              this.character = character;
+          (characters: Character[]) => {
+              this.characters = characters;
           }
       );
     this.characterService.getCharacters()
       .then(characters => {
-          this.character = characters;
+          this.characters = characters;
     })
       .catch(error => console.log(error));
   }
 
-  onNewCharacter() {
+  // tslint:disable-next-line:one-line
+  onNewCharacter(){
     this.router.navigate(['new'], { relativeTo: this.route });
   }
+
   ngOnDestroy() {
-    this.subscription.unsubscribe();
-   }
- }
+   this.subscription.unsubscribe();
+  }
+}
